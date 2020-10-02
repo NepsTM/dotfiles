5c87d32338c8410ae003ba6e45d8c1311186b562
Esteban Martin <esteban.martin@epita.fr>
2020-10-03 00:17:08 +0200

    [setup] Added link to i3 and polybar config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
 for filename in files/home/*; do
-    ln -s "$filename" ".${filename##*/}"
+    ln -sf "$PWD/$filename" "$HOME/.${filename##*/}"
+done
+
+for i in i3 polybar; do
+    mkdir -p "$HOME/.config/$i"
+    ln -sf "$PWD/files/$i/config" "$HOME/.config/$i/config"
 done
